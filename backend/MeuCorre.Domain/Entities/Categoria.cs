703ca8977f2a846dd053acc8c62983a6272c2c2b
Welton Castoldi <weltoncastoldi@hotmail.com>
2025-09-15 21:52:58 -0300

    feat(backend) #11 : adiciona entidade de categoria

--- backend/MeuCorre.Domain/Entities/Categoria.cs
+++ backend/MeuCorre.Domain/Entities/Categoria.cs
@@ -6,23 +6,50 @@ namespace MeuCorre.Domain.Entities
     {
         public Guid UsuarioId { get; private set; }
         public string Nome { get; private set; }
-        public string Descricao  { get; private set; }
-        public string Cor { get; private set; }
-        public string Icone { get; private set; }
+        public string? Descricao  { get; private set; }
+        public string? Cor { get; private set; }
+        public string? Icone { get; private set; }
         public TipoTransacao TipoDaTransacao { get; private set; }
+        public bool Ativo { get; private set; }
 
         // Propriedade de navegação para a entidade Usuario pois
         // o usuário pode ter várias categorias
         public virtual Usuario Usuario { get; private set; }
 
-        public Categoria(Guid usuarioId, string nome, string descricao, string cor, string icone, TipoTransacao tipoDaTransacao)
+        public Categoria(Guid usuarioId, string nome, TipoTransacao tipoDaTransacao, string? descricao, string? cor, string? icone)
         {
             UsuarioId = usuarioId;
-            Nome = nome;
+            Nome = nome.ToUpper();
             Descricao = descricao;
             Cor = cor;
             Icone = icone;
             TipoDaTransacao = tipoDaTransacao;
+            Ativo = true;
+        }
+
+        public void AtualizarInformacoes(string nome, TipoTransacao tipoDaTransacao, bool ativo,
+                                         string descricao, string cor, string icone)
+        {
+            Nome = nome.ToUpper();
+            Descricao = descricao;
+            Cor = cor;
+            Icone = icone;
+            TipoDaTransacao = tipoDaTransacao;
+            Ativo = ativo;
+            AtualizarDataMoficacao();
+        }
+
+        public void Ativar()
+        {
+            Ativo = true;
+            AtualizarDataMoficacao();
+        }
+
+        public void Inativar()
+        {
+            Ativo = false;
+            AtualizarDataMoficacao();
         }
     }
+
 }

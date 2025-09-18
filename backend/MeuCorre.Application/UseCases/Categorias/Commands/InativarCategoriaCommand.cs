bd86337e9e8bdffb79aae0a236ae5ba397888204
Welton Castoldi <weltoncastoldi@hotmail.com>
2025-09-18 19:51:10 -0300

    feat(backend): adiciona ativar e inativa categorias.

---
+++ backend/MeuCorre.Application/UseCases/Categorias/Commands/InativarCategoriaCommand.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using MediatR;
+using MeuCorre.Domain.Interfaces.Repositories;
+
+namespace MeuCorre.Application.UseCases.Categorias.Commands
+{
+    public class InativarCategoriaCommand : IRequest<(string, bool)>
+    {
+        [Required(ErrorMessage = "É necessário informar o ID da categoria")]
+        public required Guid CategoriaId { get; set; }
+    }
+
+    internal class InativarCategoriaCommandHandler : IRequestHandler<InativarCategoriaCommand, (string, bool)>
+    {
+        private readonly ICategoriaRepository _categoriaRepository;
+
+        public InativarCategoriaCommandHandler(ICategoriaRepository categoriaRepository)
+        {
+            _categoriaRepository = categoriaRepository;
+        }
+        
+        public async Task<(string, bool)> Handle(InativarCategoriaCommand request, CancellationToken cancellationToken)
+        {
+            var categoria =
+                await _categoriaRepository.ObterPorIdAsync(request.CategoriaId);
+
+            categoria.Inativar();
+
+            await _categoriaRepository.AtualizarAsync(categoria);
+            return ("Categoria desativada com sucesso", true);
+        }
+    }
+
+}

99d8143821fbd4ba0d1889ce28194f2927fd399b
Welton Castoldi <weltoncastoldi@hotmail.com>
2025-09-18 21:47:29 -0300

    fix(backend): corrige dependecy injection

---
+++ backend/MeuCorre/Controllers/CategoriaController.cs
@@ -0,0 +1,47 @@
+﻿using MediatR;
+using MeuCorre.Application.UseCases.Categorias.Commands;
+using MeuCorre.Application.UseCases.Usuarios.Commands;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeuCorre.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class CategoriaController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public CategoriaController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CriarCategoria([FromBody] CriarCategoriaCommad command)
+        {
+            var (mensagem, sucesso) = await _mediator.Send(command);
+            if (sucesso)
+            {
+                return Ok(mensagem);
+            }
+            else
+            {
+                return Conflict(mensagem);
+            }
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> AtualizarCategoria([FromBody] AtualizarCategoriaCommand command)
+        {
+            var (mensagem, sucesso) = await _mediator.Send(command);
+            if (sucesso)
+            {
+                return Ok(mensagem);
+            }
+            else
+            {
+                return BadRequest(mensagem);
+            }
+        }
+    }
+}
